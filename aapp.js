5e8b3c5f674ffe25a4f6aa81752b01992f31e521
faris <faris38kanbur@gmail.com>
2021-03-10 13:45:22 +0100

    add open to create or over write

--- aapp.js
+++ aapp.js
@@ -1,9 +1,24 @@
 var http = require("http");
 var fs = require("fs");
 
+// http.createServer(function(req, res) {
+//     fs.readFile("page.html", function(error, data) {
+//         if (error) throw error
+//         res.end(data);
+//     })
+// }).listen("8083")
+
+
+// http.createServer(function(req, res) {
+//         fs.appendFile("metin.txt", ", Audi",function(error) {
+//             if (error) throw error
+//             console.log("registired")
+//         })
+//     }).listen("8083")
+
+
 http.createServer(function(req, res) {
-    fs.readFile("page.html", function(error, data) {
-        if (error) throw error
-        res.end(data);
-    })
-}).listen("8083")
+    fs.open("newfile", "w", function(error){   // to create or over write
+        console.log("Created File")
+    });
+}).listen(8083)
