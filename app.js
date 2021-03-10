e9f3a3834e6ace63da36203ddff6b21b40976085
faris <faris38kanbur@gmail.com>
2021-03-10 12:06:06 +0100

    add url request

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-
-
 var http = require("http");
 
 http.createServer(function (req, res) {
     res.writeHead(200, {"Content-Type" : "text/html"});
-    res.write("<h1>Ehllo</h1>");
+    res.write(req.url); 
+
+    if (req.url == "/homepage") res.write("Welcame to main Page");
+    else if (req.url == "/login") res.write("Welcome to use Page");
+
     res.end();
-}).listen(8080);
+}).listen(8081);
