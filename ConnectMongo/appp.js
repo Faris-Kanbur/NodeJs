eb58d48620bf1bfe4ccea8d4a197477556def997
faris <faris38kanbur@gmail.com>
2021-03-11 00:35:42 +0100

    add sort method

--- ConnectMongo/appp.js
+++ ConnectMongo/appp.js
@@ -4,16 +4,21 @@ var connection = "mongodb+srv://javascriptmastery:faris5016@cluster0.yp6xf.mongo
 MongoClient.connect(connection, {useNewUrlParser:true}, function(error, db) {
     if (error) throw error;
     var dbo = db.db("database");
-    dbo.collection("müsteriler").findOne({isim: Faris}, function(err, result){ // bir tane ver cekmek icin
-        if (error) throw error;
-        console.log(result.il);
-        db.close();
-    });
+    // dbo.collection("müsteriler").findOne({isim: Faris}, function(err, result){ // bir tane ver cekmek icin
+    //     if (error) throw error;
+    //     console.log(result.il);
+    //     db.close();
+    // });
 
-    dbo.collection("müsteriler").find({}, function(err, result){ // tüm verileri cekmek icin
+    // dbo.collection("müsteriler").find({}, function(err, result){ // tüm verileri cekmek icin
+    //     if (error) throw error;
+    //     console.log(result);
+    //     db.close();
+    // });
+    
+    dbo.collection("müsteriler").find({}).sort({isim: -1}).toArray(function(err, result){ // tüm verileri cekmek icin
         if (error) throw error;
         console.log(result);
         db.close();
     });
-
 });
